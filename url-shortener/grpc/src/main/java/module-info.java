0f6de32f6815e1ac02e918bd6c5ada01dda1575d
BetweenShadows <20170874@ce.pucmm.edu.do>
2021-08-11 16:16:09 -0400

    fix: adding javax annotation and google common to module-info

--- url-shortener/grpc/src/main/java/module-info.java
+++ url-shortener/grpc/src/main/java/module-info.java
@@ -3,4 +3,7 @@ module grpc {
     requires grpc.api;
     requires grpc.core;
     requires grpc.stub;
+    requires com.google.protobuf;
+    requires javax.annotation.api;
+    requires com.google.common;
 }
