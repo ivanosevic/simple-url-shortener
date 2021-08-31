3093ae978381cf8ea581808b7a283d44f7c8f571
jean <jean18699@gmail.com>
2021-08-31 00:57:44 -0400

    cambio de nombre y nuevo atributo preview

---
+++ REST-Client/src/main/java/edu/pucmm/isc/urls/URL_POST.java
@@ -0,0 +1,17 @@
+package edu.pucmm.isc.urls;
+
+public class URL_POST {
+    private String url;
+
+    public URL_POST(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
